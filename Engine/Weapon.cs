942f03c7a2e4485964835c1e0ef051110a547450
RummenacherK <Krummenacher@live.com>
2016-11-06 15:19:50 -0600

    11/6/2016 Constructors in the Classes
    Added Constructors to the Classes for the game to clean up the code and
    further my understanding of inheritance in C#

--- Engine/Weapon.cs
+++ Engine/Weapon.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Weapon
+    public class Weapon : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int MinimumDamage { get; set; }
         public int MaxaimumDamage { get; set; }
+
+        public Weapon(int id, string name, string namePlural,
+            int minimumDamage, int maximumDamage) :
+            base(id, name, namePlural)
+        {
+            MinimumDamage = minimumDamage;
+            MaxaimumDamage = maximumDamage;
+        }
     }
 }
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -19,6 +19,29 @@ namespace SuperAdventure
         public SuperAdventure()
         {
             InitializeComponent();
+            
+            Location location = new Location(1, "Home", "This is your house");
+            /*
+            location.ID = 1;
+            location.Name = "Home";
+            location.Description = "This is your house";
+            */
+
+            _player = new Player(10, 10, 20, 0 ,1);
+
+            /*
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            */
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
         }
 
         private void btnTest_Click(object sender, EventArgs e)
